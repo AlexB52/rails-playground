a938ad4dbdb5f58e21ed01a0d5084e3405bf6fd4
Alexandre Barret <barret.alx@gmail.com>
2022-03-07 20:20:04 +1300

    Dummy app - Scaffold Comment

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :articles
   mount Playground::Engine => "/playground"
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_07_071804) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_07_071945) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_071804) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
