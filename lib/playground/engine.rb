9e3a0440bed0d5f2ba2dcd16effd597b9ff670fe
Alexandre Barret <barret.alx@gmail.com>
2022-03-08 22:44:17 +1300

    Setup to get it working on Harvest

--- lib/playground/engine.rb
+++ lib/playground/engine.rb
@@ -2,8 +2,26 @@ module Playground
   class Engine < ::Rails::Engine
     isolate_namespace Playground
 
-    initializer "blorgh.assets.precompile" do |app|
-      app.config.assets.precompile += %w( application.js application.css )
+    # If you don't want to precompile Turbo's assets (eg. because you're using webpack),
+    # you can do this in an intiailzer:
+    #
+    # config.after_initialize do
+    #   config.assets.precompile -= Turbo::Engine::PRECOMPILE_ASSETS
+    # end
+    PRECOMPILE_ASSETS = %w( application.js application.css )
+
+    initializer "playground.assets" do |app|
+      # if app.config.respond_to?(:assets)
+      #   app.config.assets.precompile += PRECOMPILE_ASSETS
+      # end
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/vendor")
     end
+
+    # initializer "playground.helpers", before: :load_config_initializers do
+    #   ActiveSupport.on_load(:action_controller_base) do
+    #     include Turbo::Streams::TurboStreamsTagBuilder, Turbo::Frames::FrameRequest, Turbo::Native::Navigation
+    #     helper Turbo::Engine.helpers
+    #   end
+    # end
   end
 end
--- playground.gemspec
+++ playground.gemspec
@@ -14,10 +14,9 @@ Gem::Specification.new do |spec|
   # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   spec.files = Dir.chdir(File.expand_path(__dir__)) do
-    Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
+    Dir["{app,config,db,lib,vendor}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
   end
 
-  spec.add_dependency "rails", ">= 7.0.2.2"
   spec.add_dependency "web-console"
   spec.add_dependency "turbo-rails"
 end
