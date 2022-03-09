2ffae7948b1037008b650f3e4bbf15326d60f236
Alexandre Barret <AlexB52@users.noreply.github.com>
2022-03-09 19:54:47 +1300

    Feature - Bundle CSS (#11)
    * Refactor npm scripts
    
    * Change location of js build
    
    * Add sass build

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-esbuild: npm run watch
+jsbuild: npm run jswatch
+cssuild: npm run csswatch
 web: bin/rails server -p 5000
--- lib/playground/engine.rb
+++ lib/playground/engine.rb
@@ -2,20 +2,8 @@ module Playground
   class Engine < ::Rails::Engine
     isolate_namespace Playground
 
-    # If you don't want to precompile Turbo's assets (eg. because you're using webpack),
-    # you can do this in an intiailzer:
-    #
-    # config.after_initialize do
-    #   config.assets.precompile -= Turbo::Engine::PRECOMPILE_ASSETS
-    # end
-    PRECOMPILE_ASSETS = %w( application.js application.css )
-
     initializer "playground.assets" do |app|
-      # if app.config.respond_to?(:assets)
-      #   app.config.assets.precompile += PRECOMPILE_ASSETS
-      # end
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/vendor")
     end
-
   end
 end
