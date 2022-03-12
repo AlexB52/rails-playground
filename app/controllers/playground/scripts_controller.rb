d950b1b8a197ee12942b20975ec7c446b3e948e9
Alexandre Barret <barret.alx@gmail.com>
2022-03-12 22:52:29 +1300

    Update the hello world example

--- app/controllers/playground/scripts_controller.rb
+++ app/controllers/playground/scripts_controller.rb
@@ -14,10 +14,22 @@ module Playground
 
     def new
       @_script = <<~SCRIPT
-        # Write Ruby as if you were in a rails console.
-        # Press cmd+Enter or ctrl+Enter on selected code to run it in the console.
+      # Access your application domain as if you were in a rails console.
+      # There are three ways to run code in the console:
 
-        1+1
+      # 1 - Select all the code from line 5 to 11 and press cmd+Enter
+      module HelloWorld
+        module_function
+
+        def hello
+          "Hello, World!"
+        end
+      end
+
+      # 2 - Put your cursor on line 14 (no selection) and press cmd+Enter
+      HelloWorld.hello
+
+      # 3 - Type "HelloWorld.hello" in the terminal box below and press cmd+Enter
       SCRIPT
 
       render :show
