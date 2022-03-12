2a6d64d484580c4ae7065b3aac6cda1348d5ce24
Alexandre Barret <AlexB52@users.noreply.github.com>
2022-03-12 16:20:21 +1300

    Feature - Style console (#12)
    * Move playgorund controller a level above in the DOM
    
    * Style the web console

--- lib/playground.rb
+++ lib/playground.rb
@@ -1,8 +1,21 @@
+require "debug"
+require "web-console"
+
+WebConsole::View.class_eval do
+  def playground_page?
+    @playground_page ||= begin
+      # {:controller=>"playground/consoles", :action=>"show"}
+      path_parameters = @env["action_dispatch.request.path_parameters"]
+      path_parameters[:controller] == "playground/consoles" &&
+      path_parameters[:action] == "show"
+    end
+  end
+end
+
+require "turbo-rails"
 require "playground/version"
 require "playground/engine"
-require "web-console"
-require "turbo-rails"
 
 module Playground
-  # Your code goes here...
+
 end
