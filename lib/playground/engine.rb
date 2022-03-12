792cd1866312ec1f462aab572594397aa21789cb
Alexandre Barret <barret.alx@gmail.com>
2022-03-12 16:16:25 +1300

    Style the web console

--- lib/playground/engine.rb
+++ lib/playground/engine.rb
@@ -2,6 +2,11 @@ module Playground
   class Engine < ::Rails::Engine
     isolate_namespace Playground
 
+    initializer "playground.web_console_templates" do |app|
+      template_paths =  File.expand_path("../templates", __FILE__)
+      WebConsole::Template.template_paths.unshift(*Array(template_paths))
+    end
+
     initializer "playground.assets" do |app|
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/vendor")
     end
